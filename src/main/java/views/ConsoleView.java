61f10033cbf3c0389f9fc7ec7092d507cf55c131
Crakchi <crakchi@gmail.com>
2025-06-10 08:22:33 -0500

    feat(UPS-002):Se agrego la view y controller (Falta el contactManager)

--- src/main/java/views/ConsoleView.java
+++ src/main/java/views/ConsoleView.java
@@ -1,4 +1,21 @@
 package views;
 
+import java.util.Scanner;
+
 public class ConsoleView {
+    public void displayMenu(){
+        System.out.println("\n**** Menu Principal ***\n");
+        System.out.println("a. Crear Contacto");
+        System.out.println("b. Buscar Contacto por Nombre");
+        System.out.println("c. Eliminar Contacto");
+        System.out.println("d. Salir");
+    }
+    public String getInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\n---\n"+prompt +" → ");
+        return scanner.next();
+    }
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
 }
